047aafd15bbc845ccfc19ee9c8ed3c44ae6c2eb5
crochet <pushpender1wayit@gmail.com>
2021-09-21 18:18:40 +0530

    commit

---
+++ Firstscript.py
@@ -0,0 +1,14 @@
+import time
+
+from selenium import webdriver
+
+driver=webdriver.Chrome("C:/Users/crochet/PycharmProjects/TestpythonProject/Chrome/chromedriver.exe")
+driver.maximize_window()
+driver.get("http://mylagrolara.1wayit.com")
+time.sleep(5)
+driver.find_element_by_xpath("//*[@id='navbarsExample09']/ul[2]/li[3]/a") .click()
+time.sleep(10)
+
+driver.close()
+#element.click()
+print("Executed")
